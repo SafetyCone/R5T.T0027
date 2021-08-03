938d4a7b9451c069872dc90714059ad2bcbc497c
David Coats <david@getsafetycone.com>
2021-08-02 22:06:34 -0700

    Add application specification startup extension for use with T009-based startups.

---
+++ source/R5T.T0027.T009/Code/Extensions/IApplicationSpecificationExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.Plymouth;
+
+
+namespace R5T.T0027.T009
+{
+    public static class IApplicationSpecificationExtensions
+    {
+        public static Task<IApplicationSpecification> UseT0027_T009_TwoStageStartup<TStartup>(this Task<IApplicationSpecification> gettingApplicationSpecification)
+            where TStartup : Startup
+        {
+            var output = gettingApplicationSpecification
+                .UseStartup<TStartup>(() =>
+                {
+                    return ApplicationBuilder.Instance
+                        .NewApplication()
+                        .UseStartup<Startup>(startupProviderServices =>
+                        {
+                            startupProviderServices.AddDefaultLogging();
+
+                            return Task.CompletedTask;
+                        })
+                        .BuildServiceProvider();
+                },
+                startupProviderServices =>
+                {
+                    startupProviderServices.AddDefaultLogging();
+
+                    return Task.CompletedTask;
+                });
+
+            return output;
+        }
+    }
+}
